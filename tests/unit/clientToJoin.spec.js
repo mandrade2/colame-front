3c638ae2b82065331c7a09d9294d1a28b13ac57b
tiborchers <tiborchers@uc.cl>
2019-04-15 13:10:31 -0400

    Fix test to check if view "ClientToJoin" has mounted

--- tests/unit/clientToJoin.spec.js
+++ tests/unit/clientToJoin.spec.js
@@ -1,9 +1,15 @@
-import { shallowMount } from '@vue/test-utils';
-import ClientToJoin from '@/components/clientToJoin.vue';
+import { createLocalVue, mount } from '@vue/test-utils';
+import ClientToJoin from '@/views/clientToJoin.vue';
+import BootstrapVue from 'bootstrap-vue';
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
 
 describe('clientToJoin.vue', () => {
   it('it mounted', () => {
-    const wrapper = shallowMount(ClientToJoin);
+    const wrapper = mount(ClientToJoin, {
+      localVue,
+    });
     const title = wrapper.find('h1');
     expect(title.text()).toMatch('Bienvenido!');
   });
