0e5c1c34f787b08a08d54a6e223f4115af8a72bb
tiborchers <tiborchers@uc.cl>
2019-04-15 13:11:01 -0400

    Created view "ClientToJoin"

---
+++ src/views/clientToJoin.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="main"
+  >
+    <h1> Bienvenido! </h1>
+    <div>
+      <b-button
+        block
+        variant="primary"
+      >
+        Unirse a una fila!
+      </b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'ClientToJoin',
+};
+</script>
+
+<style scoped>
+
+</style>
