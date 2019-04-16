86498a85b6033d078c3f14e45d3f0b57216f04a4
tiborchers <tiborchers@uc.cl>
2019-04-16 18:34:04 -0400

    Create test 'get queue information when called'

--- tests/unit/clientToJoin.spec.js
+++ tests/unit/clientToJoin.spec.js
@@ -1,22 +1,44 @@
 import { createLocalVue, mount } from '@vue/test-utils';
 import ClientToJoin from '@/views/clientToJoin.vue';
 import BootstrapVue from 'bootstrap-vue';
+import Router from '@/router';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+const mock = new MockAdapter(axios);
 
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
+localVue.use(Router);
 
 describe('clientToJoin.vue', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    mock.reset();
+    wrapper = mount(ClientToJoin, { localVue });
+  });
+
+  afterAll(() => mock.restore());
+
   it('it mounted', () => {
-    const wrapper = mount(ClientToJoin, {
-      localVue,
-    });
     const title = wrapper.find('h1');
     expect(title.text()).toMatch('Bienvenido!');
   });
   it('redirect when button is clicked', (done) => {
-    const wrapper = mount(ClientToJoin, {
-      localVue,
+    mock
+      .onGet('https://jsonplaceholder.typicode.com/users?id=1')
+      .reply(200, [{ name: 'fila', yourNumber: 10, current: 5 }]);
+    wrapper.find('button').trigger('click');
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.find('h1').exists()).toBe(false);
+      done();
     });
+  });
+  it('get queue information when called', (done) => {
+    mock
+      .onGet('https://jsonplaceholder.typicode.com/users?id=1')
+      .reply(200, [{ name: 'fila', yourNumber: 10, current: 5 }]);
     wrapper.find('button').trigger('click');
     wrapper.vm.$nextTick(() => {
       expect(wrapper.find('h1').exists()).toBe(false);
