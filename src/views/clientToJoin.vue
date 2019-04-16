72e6ff7f089827f7eaeac7a7c4d07da1b8246840
tiborchers <tiborchers@uc.cl>
2019-04-16 19:11:38 -0400

    Add Axios and error controller

--- src/views/clientToJoin.vue
+++ src/views/clientToJoin.vue
@@ -2,11 +2,21 @@
   <div
     class="main"
   >
+    <div v-if="hasError">
+      <b-alert
+        variant="danger"
+        dismissible
+        show
+      >
+        {{ error }}
+      </b-alert>
+    </div>
     <h1> Bienvenido! </h1>
     <div>
       <b-button
         block
         variant="primary"
+        @click="joinRow"
       >
         Unirse a una fila!
       </b-button>
@@ -15,12 +25,33 @@
 </template>
 
 <script>
-// @ is an alias to /src
-
+import axios from 'axios';
 
 export default {
   name: 'ClientToJoin',
+  data() {
+    return {
+      error: '',
+      hasError: false,
+    };
+  },
+  methods: {
+    joinRow() {
+      axios.get('https://jsonplaceholder.typicode.com/users?id=1')
+        .then((response) => {
+          this.$emit('queue', response.data);
+          this.hasError = false;
+          this.$router.replace('/');
+        })
+        .catch((error) => {
+          this.error = error;
+          this.hasError = true;
+        });
+    },
+  },
 };
+
+
 </script>
 
 <style scoped>
