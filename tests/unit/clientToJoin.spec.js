a0493e6060117e367b01ac9d4dfc48669850468e
tiborchers <tiborchers@uc.cl>
2019-04-16 19:11:05 -0400

    Create test 'show error'

--- tests/unit/clientToJoin.spec.js
+++ tests/unit/clientToJoin.spec.js
@@ -1,7 +1,6 @@
 import { createLocalVue, mount } from '@vue/test-utils';
 import ClientToJoin from '@/views/clientToJoin.vue';
 import BootstrapVue from 'bootstrap-vue';
-import Router from '@/router';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import flushPromises from 'flush-promises';
@@ -10,14 +9,24 @@ const mock = new MockAdapter(axios);
 
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
-localVue.use(Router);
+
+const replace = jest.fn();
+
+const $router = {
+  replace,
+};
 
 describe('clientToJoin.vue', () => {
   let wrapper;
 
   beforeEach(() => {
     mock.reset();
-    wrapper = mount(ClientToJoin, { localVue });
+    wrapper = mount(ClientToJoin, {
+      localVue,
+      mocks: {
+        $router,
+      },
+    });
   });
 
   afterAll(() => mock.restore());
@@ -25,6 +34,7 @@ describe('clientToJoin.vue', () => {
   it('it mounted', () => {
     const title = wrapper.find('h1');
     expect(title.text()).toMatch('Bienvenido!');
+    expect(wrapper.find('.alert').exists()).toBe(false);
   });
   it('redirect when button is clicked', async () => {
     mock
@@ -32,7 +42,7 @@ describe('clientToJoin.vue', () => {
       .reply(200, [{ name: 'fila', yourNumber: 10, current: 5 }]);
     wrapper.find('button').trigger('click');
     await flushPromises();
-    expect(wrapper.find('h1').exists()).toBe(false);
+    expect(replace).toHaveBeenCalledWith('/');
   });
   it('get queue information when called', async () => {
     mock
@@ -40,8 +50,18 @@ describe('clientToJoin.vue', () => {
       .reply(200, { name: 'fila', yourNumber: 10, current: 5 });
     wrapper.find('button').trigger('click');
     await flushPromises();
+    expect(wrapper.find('.alert').exists()).toBe(false);
     expect(wrapper.emitted().queue[0][0]).toMatchObject({
       name: expect.any(String), yourNumber: expect.any(Number), current: expect.any(Number),
     });
+  });
+  it('show error and dont show queue information', async () => {
+    mock
+      .onGet('https://jsonplaceholder.typicode.com/users?id=1')
+      .networkError();
+    wrapper.find('button').trigger('click');
+    await flushPromises();
+    expect(wrapper.emitted().queue).toBeUndefined();
+    expect(wrapper.find('.alert').exists()).toBe(true);
   });
 });
