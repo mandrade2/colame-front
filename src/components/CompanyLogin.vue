2c955daf5612a5e5d687adf7c0c7d03e62277519
Matías Andradde <mandrade2@uc.cl>
2019-07-03 02:44:06 -0400

    copmany login

---
+++ src/components/CompanyLogin.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="main">
+    <div v-if="hasError">
+      <b-alert variant="danger" dismissible show>{{ error }}</b-alert>
+    </div>
+    <h1 class="title">Bienvenido!</h1>
+    <b-form-input v-model="username" />
+    <b-form-input type="password" v-model="password" />
+    <div>
+      <b-button block class="begin" variant="light" @click="login(username,password)">Ingresar</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "CompanyLogin",
+  data() {
+    return {
+      error: "",
+      hasError: false,
+      username: "",
+      password: ""
+    };
+  },
+  mounted() {},
+  methods: {
+    login(username, password) {
+      console.log(username, password);
+      axios
+        .post(`http://127.0.0.1:3000/company`, { username, passowrd })
+        .then(() => {
+          this.$emit("status", {
+            status: "AlreadyLogged"
+          });
+          this.hasError = false;
+        })
+        .catch(error => {
+          this.error = error;
+          this.hasError = true;
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.main {
+  margin: 0 auto;
+  padding-top: 25vh;
+}
+.title {
+  padding-top: 25 vh;
+  font-size: 400%;
+}
+
+.begin {
+  width: 70%;
+  margin: 10vh auto;
+}
+</style>
--- src/views/CompanyView.vue
+++ src/views/CompanyView.vue
@@ -1,43 +1,53 @@
 <template>
   <div class="main">
-    <b-tabs
-      vertical
-      nav-wrapper-class="text-danger"
-      active-nav-item-class="text-danger"
-      nav-class="bg-light font-weight-bold text-danger"
-    >
-    <b-tab title="Colame" disabled><p>I'm a disabled tab!</p></b-tab>
-      <b-tab
-        title="Lineas"
-        active
+    <div v-if="status == 'notLoggedIn'">
+      <CompanyLogin @status="updateStatus" />
+    </div>
+    <div v-if="status != 'notLoggedIn'">
+      <b-tabs
+        vertical
+        nav-wrapper-class="text-danger"
+        active-nav-item-class="text-danger"
+        nav-class="bg-light font-weight-bold text-danger"
       >
-        <CompanyLinesList />
-      </b-tab>
-      <b-tab title="Atendedores">
-        <CompanyAttendantList />
-      </b-tab>
-    </b-tabs>
+        <b-tab title="Colame" disabled>
+          <p>I'm a disabled tab!</p>
+        </b-tab>
+        <b-tab title="Lineas" active>
+          <CompanyLinesList />
+        </b-tab>
+        <b-tab title="Atendedores">
+          <CompanyAttendantList />
+        </b-tab>
+      </b-tabs>
+    </div>
   </div>
 </template>
 
 <script>
-import CompanyAttendantList from '@/components/CompanyAttendantList.vue';
-import CompanyLinesList from '@/components/Companylinelist.vue';
+import CompanyAttendantList from "@/components/CompanyAttendantList.vue";
+import CompanyLinesList from "@/components/Companylinelist.vue";
+import CompanyLogin from "@/components/CompanyLogin.vue";
 
 export default {
-  name: 'CompanyView',
+  name: "CompanyView",
   components: {
     CompanyAttendantList,
     CompanyLinesList,
+    CompanyLogin
   },
+  data() {
+    return {
+      error: "",
+      hasError: false,
+      status: "notLoggedIn"
+    };
+  }
 };
-
-
 </script>
 
 <style scoped>
 .main {
   margin: auto;
 }
-
 </style>
